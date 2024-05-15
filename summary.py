82896395e2e8d14c3041680ec2f1ee26a75bab5b
Liselotte Van Ombergen <liselotte.vanombergen@student.hogent.be>
2024-05-15 10:47:53 +0200

    corr

--- summary.py
+++ summary.py
@@ -1,17 +1,15 @@
 
 import openai
 
-
-
 def summarize(legal_questions, max_words):
     client = openai()
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
     {"role": "system", "content": "Je bent een jurist"},
-    {"role": "user", "content": f"Vermeld de wetsartikels en juridisch principes uit {legal_questions}. 
+    {"role": "user", "content": f"""Vermeld de wetsartikels en juridisch principes uit {legal_questions}. 
 Je output bedraagt maximum {max_words} woorden, en bevat geen vermelding van de specifieke partijen, feiten of details van de zaak. 
-Presenteer de tekst als een doorlopende tekst zonder enige onderverdelingen of ondertitels."}
+Presenteer de tekst als een doorlopende tekst zonder enige onderverdelingen of ondertitels."""}
     ]
   
 )
