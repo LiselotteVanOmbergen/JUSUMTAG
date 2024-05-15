49ee2a8ad7ac203ab5a3669a1e2f1c497d847ffe
Liselotte Van Ombergen <liselotte.vanombergen@student.hogent.be>
2024-05-15 10:54:52 +0200

    corr

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import os
 import openai
+from legal_questions import define_legal_questions
 from summary import summarize
 from tagging import tag
-from legal_questions import define_legal_questions
+
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY", st.secrets.get("OPENAI_API_KEY"))
@@ -11,18 +12,20 @@ openai.api_key = os.getenv("OPENAI_API_KEY", st.secrets.get("OPENAI_API_KEY"))
 col1, col2 = st.columns(2)
 
 with col1:
-
     st.header("Laad vonnis of arrest op")
     file = st.file_uploader()
+    if file:
+        legal_questions = define_legal_questions(file)
 
 with col2:
-    
-    if st.button("Beknopte samenvatting (max. 150)"):
-        legal_questions = define_legal_questions(file)
-        st.write(summarize(legal_questions, 150)),
-        legal_questions = define_legal_questions(file)
-    if st.button("Uitvoerige samenvatting (max. 300)"):
-        legal_questions = define_legal_questions(file)
-        st.write(summarize(legal_questions, 300)),
-    if st.button('Genereer tags'):
-        st.write(tag(legal_questions))
+    if file:
+        if st.button("Beknopte samenvatting (max. 150)"):
+            st.write(summarize(legal_questions, 150))
+
+        if st.button("Uitvoerige samenvatting (max. 300)"):
+            st.write(summarize(legal_questions, 300))
+
+        if st.button('Genereer tags'):
+            st.write(tag(legal_questions))
+    else:
+        st.write("Laad een bestand op om samenvattingen en tags te genereren.")
