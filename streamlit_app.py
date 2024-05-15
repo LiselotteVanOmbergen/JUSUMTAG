64d109aa732edf20ed95f203f82f9e6b8ccc8312
Liselotte Van Ombergen <liselotte.vanombergen@student.hogent.be>
2024-05-15 14:10:40 +0200

    CH

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ if "judgment" not in st.session_state:
 # Upload Text Section
 st.header("Upload Tekst")
 text_area_judgment = st.text_area(label="Plak hieronder de tekst van een vonnis of arrest")
-col1, col2 = st.columns(2)
-with col1:
-    if st.button("Tekst opladen :spiral_note_pad:"):
+
+
+if st.button("Tekst opladen :spiral_note_pad:"):
         if text_area_judgment:
             st.session_state.judgment = text_area_judgment
             st.session_state.legal_questions = define_legal_questions(text_area_judgment)
@@ -35,24 +35,16 @@ with col1:
         else:
             st.write("Geen tekst opgeladen")
 
-# Add separator
-st.write("---")
 
-# Summarize Section
-st.header("Samenvatting")
+col1, col2, col3 = st.columns(3)
+
 with col1:
     if st.button("Beknopte samenvatting (max. 150):female-judge:"):
         st.session_state.summary_short = summarize(st.session_state.legal_questions, 150, st.session_state.judgment)
 with col2:
     if st.button("Uitvoerige samenvatting (max. 300):female-judge:"):
         st.session_state.summary_long = summarize(st.session_state.legal_questions, 300, st.session_state.judgment)
-
-# Add separator
-st.write("---")
-
-# Tags Section
-st.header("Tags")
-with col1:
+with col3:
     if st.button('Genereer tags :female-judge:'):
         st.session_state.tags = tag(st.session_state.legal_questions)
 
