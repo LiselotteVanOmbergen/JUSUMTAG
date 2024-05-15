e269f7de9bfa51caf63b6d57939147f402817d62
Liselotte Van Ombergen <liselotte.vanombergen@student.hogent.be>
2024-05-15 11:21:54 +0200

    corr

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,28 +5,38 @@ from legal_questions import define_legal_questions
 from summary import summarize
 from tagging import tag
 
-
-
 openai.api_key = os.getenv("OPENAI_API_KEY", st.secrets.get("OPENAI_API_KEY"))
 
 col1, col2 = st.columns(2)
 
+file = st.text_input(label="")
+
+legal_questions = None
+summary_short = ""
+summary_long = ""
+tags = ""
+
+if file:
+    legal_questions = define_legal_questions(file)
+
 with col1:
     st.header("Laad vonnis of arrest op")
-    #file = st.file_uploader(label = "")
-    file = st.text_input(label = "")
     if file:
-        legal_questions = define_legal_questions(file)
+        st.write("File loaded:", file)
+    else:
+        st.write("No file loaded.")
 
 with col2:
     if file:
         if st.button("Beknopte samenvatting (max. 150)"):
-            st.write(summarize(legal_questions, 150))
+            summary_short = summarize(legal_questions, 150)
 
         if st.button("Uitvoerige samenvatting (max. 300)"):
-            st.write(summarize(legal_questions, 300))
+            summary_long = summarize(legal_questions, 300)
 
         if st.button('Genereer tags'):
-            st.write(tag(legal_questions))
-    else:
-        st.write("")
+            tags = tag(legal_questions)
+
+    st.write(summary_short)
+    st.write(summary_long)
+    st.write(tags)
--- tagging.py
+++ tagging.py
@@ -1,7 +1,7 @@
 
 import openai
 
-voorbeeld = """BURGERLIJK RECHT - VERBINTENIS UIT ONRECHTMATIGE DAAD - Begroting schade - Arbeidsongeschiktheid - Tijdelijke invaliditeit
+voorbeeld = """Begroting schade - Arbeidsongeschiktheid - Tijdelijke invaliditeit
 HANDELS-, ECONOMISCH EN FINANCIEEL RECHT - VERZEKERINGEN - Publiek verzekeringsrecht -	
 Gebrekkige zaak - schade conform artikel 1384 eerste lid BW - tijdelijke arbeidsongeschiktheid - personeelslid - publieke sector"""
 
@@ -11,8 +11,9 @@ def tag(legal_questions):
     model="gpt-3.5-turbo",
     messages=[
     {"role": "system", "content": "Je bent een jurist."},
-    {"role": "user", "content": f"""Geef op basis van {legal_questions} de op juridisch vlak belangrijkste sleutelwoorden van het vonnis of arrest, conform het gegeven voorbeeld, zonder specifieke namen of feiten te gebruiken. 
+    {"role": "user", "content": f"""Geef op basis van {legal_questions} de op juridisch vlak belangrijkste sleutelwoorden van het vonnis of arrest, zonder specifieke namen of feiten te gebruiken. 
 Zorg ervoor dat de sleutelwoorden helder en beknopt zijn, zodat een jurist snel kan inschatten waar het vonnis of arrest over handelt. 
+Volg de exact de structuur van het gegeven voorbeeld.
 Scheid de woorden door middel van een koppelteken "-". 
 Voorbeeld: {voorbeeld.lower}"""}
     ]
